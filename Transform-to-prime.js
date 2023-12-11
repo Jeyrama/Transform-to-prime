80254a8cc05a21e21730153dd5983bbfe982446f
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-11 13:33:14 -0800

    Update Transform-to-prime.js

--- Transform-to-prime.js
+++ Transform-to-prime.js
@@ -50,3 +50,25 @@ function minimumNumber(numbers) {
 }
 
 // or
+
+function isPrime (n) {
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function minimumNumber(numbers){
+  let count = 0;
+  let sum = numbers.reduce((a,b) => a+b);
+
+  if (isPrime(sum)) return 0;
+
+  while(!isPrime(sum)) {
+    sum++;
+    count++;
+  }
+  return count;
+}
