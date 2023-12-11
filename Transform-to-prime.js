37700ca399a8c0792a2a7a85b34be3ed3a3f22b9
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-11 13:16:03 -0800

    Update Transform-to-prime.js

--- Transform-to-prime.js
+++ Transform-to-prime.js
@@ -33,3 +33,18 @@ Input >> Output Examples:
 
 
 // Solution
+
+function minimumNumber(numbers) {
+  let sum = numbers.reduce((a,b) => a + b)
+  for (let i = sum; ; i++) {
+    if(prime(i)) return i - sum
+  }
+  function prime(a) {
+    if(a < 2) return false
+    if(a % 2 === 0) return a === 2
+    for(let i = 3; i * i <= a; i += 2) {
+      if(a % i === 0) return false
+    }
+    return true
+  }
+}
